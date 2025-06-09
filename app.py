bb4a72776baa15e75428f22bc4c8463475414aaa
davbk <davidc828@nycstudents.net>
2025-06-09 09:47:53 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,27 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, render_template
 import requests
 
 app = Flask(__name__)
 
 @app.route("/")
-def show_games():
-    response = requests.get("https://www.freetogame.com/api/games?category=shooter")
-    
-    if response.status_code != 200:
-        return "Failed to fetch games", 500
+def show_route():
+    response = requests.get("https://data.cityofnewyork.us/resource/ycrg-ses3.json")
 
-    games = response.json()
-    return render_template("index.html", games=games)
+    if response.status_code != 200:
+        return "Failed to fetch routes", 500
+
+    routes = response.json()
+    return render_template("index.html", routes=routes)
 
 @app.route("/games/<int:game_id>")
-def game_details(game_id):
+def route_details(game_id):
     response = requests.get(f"https://www.freetogame.com/api/game?id={game_id}")
 
     if response.status_code != 200:
         return "Failed to fetch game details", 500
 
-    game = response.json()
-    return render_template("items.html", game=game)
+    route = response.json()
+    return render_template("items.html", route=route) 
 
 if __name__ == "__main__":
     app.run(debug=True)
